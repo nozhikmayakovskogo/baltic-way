29c3ae2ae60ba644820c08b5406580d0079df743
Neretnieks Raņķis <nozhikmayakovskogo@gmail.com>
2022-10-19 13:22:40 +0300

    Added favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,23 +1,22 @@
 import { defineNuxtConfig } from 'nuxt'
-
 export default defineNuxtConfig({
-    title: 'Nuxt App',
-    meta: {
-        link: [
-            {
-                rel: 'stylesheet',
-                href: '/assets/css/mdb.min.css'
-            }
-        ],
-        script: [
-            {
-                src: '/assets/js/mdb.min.js',
-                defer: true 
-            },
-            {
-                src: '/assets/js/main.js',
-                defer: true 
-            }
-        ]
-    }
+  title: 'Baltic Way',
+  meta: {
+    link: [
+      {
+        rel: 'stylesheet',
+        href: '/assets/css/mdb.min.css'
+      }
+    ],
+    script: [
+      {
+        src: '/assets/js/mdb.min.js',
+        defer: true 
+      },
+      {
+        src: '/assets/js/main.js',
+        defer: true 
+      }
+    ]
+  }
 })
